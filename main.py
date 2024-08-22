e35cb375cdd45cc055422ed677e9035173f07e39
Arpit Pathak <119810812+Thepathakarpit@users.noreply.github.com>
2024-08-22 22:35:31 +0530

    Update main.py
    Added functionality to read the current path and use it to execute batch files.

--- main.py
+++ main.py
@@ -6,22 +6,26 @@ from subprocess import Popen
 api = ""
 genai.configure(api_key=api)
 
-model = genai.GenerativeModel('gemini-1.0-pro-latest')
-ques = input("Please enter your prompt: ")
-#Pro tip: You can do anything at cmd using bat file so use your imagination and problemsolving you can do almost everything. wink wink! You can create and run files including code files like py, install and use libraries, think... 
+model = genai.GenerativeModel('gemini-1.5-flash')
+def main():
+  ques = input("Please enter your prompt: ")
+  #Pro tip: You can do anything at cmd using bat file so use your imagination and problemsolving you can do almost everything. wink wink! You can create and run files including code files like py, install and use libraries, think... 
 
-#Prompt is set to generate a batch file for the given task but you can modify it accordingly if you want.
-response = model.generate_content("Generate batch file to automate the task I am about to tell you. Don't output any additional symbols or any comments straight up executable batch file.:Please Generate a batch file that "+ques)
+  #Prompt is set to generate a batch file for the given task but you can modify it accordingly if you want.
+  response = model.generate_content("Generate batch file to automate the task I am about to tell you. Don't output any additional symbols or any comments straight up executable batch file.:Please Generate a batch file that"+ques)
 
-if os.path.exists("file.txt"):
-  os.remove("file.txt")
+  if os.path.exists("file.txt"):
+    os.remove("file.txt")
 
-if os.path.exists("file.bat"):
-  os.remove("file.bat")
-f = open("file.bat", "x")
-f = open("file.bat", "a")
-f.write(response.text)
-f.close()
+  if os.path.exists("file.bat"):
+    os.remove("file.bat")
+  f = open("file.bat", "x")
+  f = open("file.bat", "a")
+  f.write(response.text)
+  f.close()
 
-p = Popen("file.bat", cwd="C:/Users/patha/OneDrive/Documents/code/Batman/book-rec/")
-stdout, stderr = p.communicate()
+  path = os.getcwd()
+  p = Popen("file.bat", cwd=path)
+  stdout, stderr = p.communicate()
+while(1):
+  main()
